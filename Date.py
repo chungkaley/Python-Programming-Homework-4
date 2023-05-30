22a95d1001440d58fa407a9619b8ccf36699eb27
chungkaley <125612528+chungkaley@users.noreply.github.com>
2023-05-29 21:24:02 -0700

    Update Date.py

--- Date.py
+++ Date.py
@@ -1,1 +1,7 @@
-
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month =month
+        self.year = year
+    def __str__(self):
+        return f"{self.day}/{self.month}/{self.year}"
